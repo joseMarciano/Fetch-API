e0f23356905a6087e63596aa462d792c83e8b91b
zezeka <marcianojosepaulo@gmail.com>
2020-10-02 20:10:07 -0300

    02.Consumindo dados da API | CAP: 07 - Exibindo os dados do cliente

--- api/cliente.js
+++ api/cliente.js
@@ -3,5 +3,4 @@ const response = fetch('http://localhost:4000/clientes');
 /* O fetch retorna uma promisse .then(pego o json da resposta)
 .then(retorno o json que vem como um array de objetos)
 */
-// eslint-disable-next-line no-console
-response.then((params) => params.json()).then((json) => console.log(json));
+const listarClientes = () => response.then((params) => params.json()).then((json) => json);
---
+++ listagem-cliente.js
@@ -0,0 +1,17 @@
+const bodyTable = document.querySelector('[data-body-table]');
+
+const showClient = (name, cpf) => {
+    const line = document.createElement('tr');
+    const lineContent = `
+    <td>${cpf}</td>
+    <td>${name}</td>
+    `;
+
+    line.innerHTML = lineContent;
+    return line;
+};
+
+// eslint-disable-next-line no-undef
+listarClientes().then((clients) => {
+    clients.forEach((client) => bodyTable.appendChild(showClient(client.nome, client.cpf)));
+});
